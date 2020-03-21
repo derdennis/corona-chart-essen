3fdb1f273cc8e31146b1117454d03b7beef771b3
Dennis Wegner <dennis@instant-thinking.de>
2020-03-21 13:47:31 +0100

    Beautified all the files.

--- corona-essen-linechart.js
+++ corona-essen-linechart.js
@@ -1,6 +1,11 @@
 var parseDate = d3.timeParse("%Y-%m-%d");
 
-var margin = {left: 50, right: 20, top: 20, bottom: 50 };
+var margin = {
+    left: 50,
+    right: 20,
+    top: 20,
+    bottom: 50
+};
 
 var width = 800 - margin.left - margin.right;
 var height = 500 - margin.top - margin.bottom;
@@ -14,46 +19,63 @@ var minDate = new Date();
 var maxDate = new Date();
 
 d3.csv("cases.csv")
-    .row(function(d) { return { date: parseDate(d.date), cases: Number(d.cases)}; })
-    .get(function(error, rows) {
-	    max = d3.max(rows, function(d) { return d.cases; });
-	    minDate = d3.min(rows, function(d) {return d.date; });
-		maxDate = d3.max(rows, function(d) { return d.date; });
+    .row(function (d) {
+        return {
+            date: parseDate(d.date),
+            cases: Number(d.cases)
+        };
+    })
+    .get(function (error, rows) {
+        max = d3.max(rows, function (d) {
+            return d.cases;
+        });
+        minDate = d3.min(rows, function (d) {
+            return d.date;
+        });
+        maxDate = d3.max(rows, function (d) {
+            return d.date;
+        });
 
 
-		var y = d3.scaleLinear()
-					.domain([0,max])
-					.range([height,0]);
+        var y = d3.scaleLinear()
+            .domain([0, max])
+            .range([height, 0]);
 
-		var x = d3.scaleTime()
-					.domain([minDate,maxDate])
-					.range([0,width]);
+        var x = d3.scaleTime()
+            .domain([minDate, maxDate])
+            .range([0, width]);
 
-		var yAxis = d3.axisLeft(y);
+        var yAxis = d3.axisLeft(y);
 
-		var xAxis = d3.axisBottom(x);
+        var xAxis = d3.axisBottom(x);
 
-		var line = d3.line()
-			.x(function(d){ return x(d.date); })
-			.y(function(d){ return y(d.cases); })
-			.curve(d3.curveCardinal);
-
-
-		var svg = d3.select("#chart").append("svg").attr("id","svg").attr("height","100%").attr("width","100%");
-		var chartGroup = svg.append("g").attr("class","chartGroup").attr("transform","translate("+xNudge+","+yNudge+")");
-
-		chartGroup.append("path")
-			.attr("class","line")
-			.attr("d",function(d){ return line(rows); })
+        var line = d3.line()
+            .x(function (d) {
+                return x(d.date);
+            })
+            .y(function (d) {
+                return y(d.cases);
+            })
+            .curve(d3.curveCardinal);
 
 
-		chartGroup.append("g")
-			.attr("class","axis x")
-			.attr("transform","translate(0,"+height+")")
-			.call(xAxis);
+        var svg = d3.select("#chart").append("svg").attr("id", "svg").attr("height", "100%").attr("width", "100%");
+        var chartGroup = svg.append("g").attr("class", "chartGroup").attr("transform", "translate(" + xNudge + "," + yNudge + ")");
 
-		chartGroup.append("g")
-			.attr("class","axis y")
-			.call(yAxis);
+        chartGroup.append("path")
+            .attr("class", "line")
+            .attr("d", function (d) {
+                return line(rows);
+            })
 
-	});
+
+        chartGroup.append("g")
+            .attr("class", "axis x")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        chartGroup.append("g")
+            .attr("class", "axis y")
+            .call(yAxis);
+
+    });
