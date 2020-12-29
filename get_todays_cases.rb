2e0840bb476f087882d4daf34a34d076c3c409a1
Dennis Wegner <dennis@instant-thinking.de>
2020-12-29 16:09:10 +0100

    Now at 1744 cases.

--- get_todays_cases.rb
+++ get_todays_cases.rb
@@ -43,8 +43,8 @@ page                = Nokogiri::HTML(open(CORONA_UPDATES_IN_ESSEN))
 # Getting todays date in the two required formats
 todays_date_german  = Time.now.strftime("%d.%m.%Y")
 todays_date_iso     = Time.now.strftime("%Y-%m-%d")
-#todays_date_german  = "26.12.2020"
-#todays_date_iso     = "2020-12-26"
+#todays_date_german  = "28.12.2020"
+#todays_date_iso     = "2020-12-28"
 
 # Getting all paragraphs from the website, stepping through them
 page.css('p').each{ |p|
